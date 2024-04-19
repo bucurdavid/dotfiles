73358ca4f784d1caed23ff4f1be9fd198e99c6a1
Bucur David <dbucur22@gmail.com>
2024-04-19 23:11:06 +0300

    chore: aliases update

--- aliases.zsh
+++ aliases.zsh
@@ -17,19 +17,23 @@ alias sol-dev="cd ~/dev/sol"
 
 
 #git 
-alias gl="git pull --prune"
+alias l="git pull --prune"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp="git push"
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
-alias ga="git add ." 
-alias gc="git commit"
-alias gcm="git commit -m"
-alias gco="git checkout"
-alias gb="git branch"
-alias gs='git status -sb'
-alias gac="git add -A && git commit -m"
-alias gp="git push"
+alias a="git add ." 
+alias c="git commit"
+alias ap="git add -p"
+alias cm="git commit -m"
+alias m="git merge"
+alias co="git checkout"
+alias cob="git checkout -b"
+alias s='git status -sb'
+alias acm="git add -A && git commit -m"
+alias p="git push"
+alias pf="git push --force"
+alias st="git stash"
+alias stp="git stash pop"
 
 
 #docker
@@ -56,3 +60,5 @@ alias cb="cargo build"
 
 alias sol="solana"
 alias ab="anchor build"
+alias ai="anchor init"
+alias ad="anchor deploy"
